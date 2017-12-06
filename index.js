6d90af34a77f0836a8070104020902c08c5d1668
Tossaphol Chintanaphan <tossaphol.chi@krungthai-axa.co.th>
2017-12-06 15:05:20 +0700

    Mongoose Part 2

--- index.js
+++ index.js
@@ -10,26 +10,24 @@ const connect = mongoose.connect(url, {
 
 connect.then((db) => {
   console.log('Connected correctly to server');
-  var newDish = Dishes({
+  Dishes.create({
     name: 'Pizza',
     description: 'test'
+  })
+  .then((dish) => {
+    console.log(dish);
+
+    return Dishes.find({}).exec();
+  })
+  .then((dishes) => {
+    console.log(dishes);
+
+    return db.collection('dishes').drop();
+  })
+  .then(() => {
+    return db.close();
+  })
+  .catch((err) => {
+    console.log(err);
   });
-
-  newDish.save()
-    .then((dish) => {
-      console.log(dish);
-
-      return Dishes.find({}).exec();
-    })
-    .then((dishes) => {
-      console.log(dishes);
-
-      return db.collection('dishes').drop();
-    })
-    .then(() => {
-      return db.close();
-    })
-    .catch((err) => {
-      console.log(err);
-    });
 });
